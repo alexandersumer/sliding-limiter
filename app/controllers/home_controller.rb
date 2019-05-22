7472c351d43ea59c9c631f70813a6e3a6d25a388
Alexander Jones <alexanderj2357@gmail.com>
2019-05-22 10:51:07 +1000

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,12 @@
 # app/controllers/home_controller.rb
 
-require_relative '../lib/rate_limiter/services/redis_cache'
-require_relative '../lib/rate_limiter/services/local_mem_cache'
-require_relative '../lib/rate_limiter/services/cache_client'
-
 class HomeController < ActionController::Base
 	before_action :rate_limit
 
 	include RateLimiter
 
 	def rate_limit
-		threshold = THRESHOLD
-		interval = INTERVAL
-		accuracy = ACCURACY
-		redis_cache = RedisCache.new
-		local_mem_cache = LocalMemCache.new
-
-		limiter = RateLimiter::LimiterClient.new(
-			"handle_authentication_requests", threshold, interval, accuracy, CacheClient.new(redis_cache)
-		)
+		limiter = RateLimiter::LimiterClient.new("handle_authentication_requests", THRESHOLD, INTERVAL, ACCURACY)
 
 		if limiter.is_blocked?(request.ip)
 			render status: TOO_MANY_REQUESTS, plain: limiter.get_error_message(request.ip)
